c00e2151424a557f31d0301edc9d930fe2d5a1ae
Felipe Cardoso <felipepassoscardoso@gmail.com>
2021-03-26 20:57:59 -0300

    Alterando settings.py para produção

--- estacao/settings.py
+++ estacao/settings.py
@@ -1,3 +1,4 @@
+# False para Desenvolvimento
 PRODUCTION=True
 
 if PRODUCTION:
@@ -6,22 +7,29 @@ else:
     DB_PATH='estacao.db'
 
 PREFIX_API_VERSION = '/api/v1'
-API_PORT = 5000
-API_DEBUG = True
+API_PORT = 5000                         # Se alterar a Porta da API aqui, deve-se alterar no arquivo uwsgi.ini também.
+API_DEBUG = False
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DB_PATH
 
-RABBIT_SERVER = {
-    'HOST' : 'us127.serverdo.in',
-    'PORT' : 5672,
-    'USER' : 'admin',
-    'PASS' : 'pji29006'
-}
-EXCHANGE_NOTIFICATIONS='notifications'
-
-SERVICE_NOTIFICATION = True
-SERVICE_MESSAGE = True
 LOGGING_CONF = 'logging.ini'
+
+SERVICE_MESSAGE = True
+SERVICE_NOTIFICATION = True
+
+# Altere para informações do Servidor de Fila de Mensagens (Broker)
+# Projeto utiliza o broker do RabbitMQ
+RABBIT_SERVER = {
+    'HOST' : 'broker.example.com',       # Ou IP do servidor.
+    'PORT' : 5672,                       # Porta para conexão, padrão do RabbitMQ é 5672.
+    'USER' : 'User',                     # Usuário do servidor de mensagens.
+    'PASS' : 'Pass'                      # Senha do Usuário informando.
+}
+EXCHANGE_NOTIFICATIONS='notifications'   # Exchange do Servidor RabbitMQ para envio das notificações.
+
+
+
+
 
 

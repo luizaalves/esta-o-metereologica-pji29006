320ef5b6fbe744e3e6cccc0dcde154338bb156e8
Felipe Cardoso <felipepassoscardoso@gmail.com>
2021-04-14 21:50:17 -0300

    add example_driver.py

---
+++ estacao/modules/driver_example.py
@@ -0,0 +1,17 @@
+from modules.interfaces import IModule
+from random import randint
+
+class Example(IModule):
+    def __init__(self):
+        self.active = True
+       
+    def read(self, type_grandeza: str):
+        if type_grandeza.upper() == "Temperatura":
+            return randint(0,100)
+        elif type_grandeza.upper() == "Pressure":
+            return randint(300,3000)
+        else:
+            return randint(0,50)
+    
+    def start(self):
+        print("Iniciando {}".format(self.__class__))

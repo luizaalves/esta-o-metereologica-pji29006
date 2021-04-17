fc6af836f3bcb610fa1a4f2e8a6f4248db2e4b31
Felipe Cardoso <felipepassoscardoso@gmail.com>
2021-04-17 18:12:09 +0100

    release 3.0.0

--- estacao/settings.py
+++ estacao/settings.py
@@ -22,10 +22,10 @@ SERVICE_NOTIFICATION = True
 # Altere para informações do Servidor de Fila de Mensagens (Broker)
 # Projeto utiliza o broker do RabbitMQ
 RABBIT_SERVER = {
-    'HOST' : 'us127.serverdo.in',              # FQDN ou IP do servidor.
+    'HOST' : 'FQDN.broker',              # FQDN ou IP do servidor.
     'PORT' : 5672,                       # Porta para conexão, padrão do RabbitMQ é 5672.
-    'USER' : 'admin',                     # Usuário do servidor de mensagens.
-    'PASS' : 'pji29006'                  # Senha do Usuário informando.
+    'USER' : 'user',                     # Usuário do servidor de mensagens.
+    'PASS' : 'password'                  # Senha do Usuário informando.
 }
 EXCHANGE_NOTIFICATIONS='notifications'   # Exchange do Servidor RabbitMQ para envio das notificações (Publish/Subscribe).
 EXCHANGE_TYPE='fanout'                   # Tipo de Exchange do servidor RabbitMQ (Publish/Subscribe).
@@ -41,7 +41,7 @@ EXCHANGE_MESSAGES=''                     # Exchange do Servidor RabbitMQ para Se
 # Verificar endereço da interface na Rasp - sudo i2cdetect -y 1
 # Se o comando acima não retornar nada é possível que a interface I2C na rasp não esteja habilitada.
 # Para habilitar execute - sudo raspi-config - e escolha as opções 5 e P5.
-I2C_ADDRESS = 0x77                       # Algumas placas o endereço é 0x77
+I2C_ADDRESS = 0x76                       # Algumas placas o endereço é 0x77
 SEA_LEVEL_PRESSURE = 1020.00             # Pressao nivel do mar em Florianopolis
 USE_BMP280=True                          # Informe True se deseja utilizar o módulo BMP280 implementado por padrão.
 
